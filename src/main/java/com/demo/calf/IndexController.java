6913f886faf0cb0844abf90cdfd259230637148f
lihongde <lihongde@leweikeji.com>
2019-06-25 23:14:44 +0800

    java map : Hashtable

--- src/main/java/com/demo/calf/IndexController.java
+++ src/main/java/com/demo/calf/IndexController.java
@@ -1,6 +1,7 @@
 package com.demo.calf;
 
 import com.demo.calf.data.CollectionDemo;
+import com.demo.calf.data.MapDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,9 @@ public class IndexController {
 
     @Autowired
     CollectionDemo collectionDemo;
+
+    @Autowired
+    MapDemo mapDemo;
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
@@ -23,7 +27,8 @@ public class IndexController {
         // collectionDemo.LinkedList();
         // collectionDemo.HashSet();
         // collectionDemo.TreeSet();
-        collectionDemo.Queue();
+        // collectionDemo.Queue();
+        mapDemo.Hashtable();
     }
 
 }
--- src/main/java/com/demo/calf/data/MapDemo.java
+++ src/main/java/com/demo/calf/data/MapDemo.java
@@ -2,8 +2,38 @@ package com.demo.calf.data;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 @Service
 
 public class MapDemo {
+
+    public String Hashtable(){
+        Hashtable balance = new Hashtable();
+        Enumeration names;
+        String str;
+        double bal;
+
+        balance.put("PHP",new Double(3434.34));
+        balance.put("Java",new Double(123.22));
+        balance.put("Python",new Double(1378.00));
+        balance.put("Golang",new Double(99.22));
+        balance.put("Kotlin",new Double(-19.08));
+
+        names = balance.keys();
+        System.out.println(names);
+
+        while(names.hasMoreElements()) {
+            str = (String) names.nextElement();
+            System.out.println(str + " : " + balance.get(str));
+        }
+
+        bal = ((Double)balance.get("Java")).doubleValue();
+        balance.put("Java",new Double(bal + 1000));
+        System.out.println("Java's new balance : " + balance.get("Java"));
+
+        return "";
+    }
 
 }
